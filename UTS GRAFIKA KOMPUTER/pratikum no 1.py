28535876ded99422abf2b6f250868819d0c2a042
Iqbal1606 <72688860+Iqbal1606@users.noreply.github.com>
2022-04-01 10:46:24 +0700

    Add files via upload

---
+++ UTS GRAFIKA KOMPUTER/pratikum no 1.py
@@ -0,0 +1,36 @@
+# Nama  : IQBAL HARIO SYAHPUTRA
+# NIM   : 20051397029
+# Kelas : D4 MI 2020A
+# UTS PRAKTIKUM GRAFIKA KOMPUTER
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-6, 6, -6, 6, -1, 1)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glColor3f(1, 1, 1)
+    glBegin(GL_LINE_STRIP)
+    glVertex2f(-4.00, 5.00)
+    glVertex2f(0.0, 0.00)
+    glVertex2f(4.00, 6.00)
+    glEnd()
+
+    glutSwapBuffers()
+
+
+glutInit(sys.argv)
+glutInitWindowSize(500, 500)
+glutCreateWindow("UTS GRAFIKA KOMPUTER")
+glutDisplayFunc(draw)
+glutMainLoop()
